f5b7d8decf6702e4cbd74d1c05dfc4f9f1a900a4
PaulDiTi <st161607@stud.uni-stuttgart.de>
2022-03-26 18:42:47 +0100

    Created StoreInvitationRequest.php

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AcceptInvitationRequest;
+use App\Http\Requests\StoreInvitationRequest;
 use App\Mail\InvitationCreated;
 use App\Models\Invitation;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -61,10 +63,10 @@ class InvitationController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @param StoreInvitationRequest $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(StoreInvitationRequest $request)
     {
         $admin_user = Auth::user();
         $permissions = $request->permissions;
@@ -105,7 +107,7 @@ class InvitationController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Invitation  $invitation
-     * @return \Illuminate\Http\RedirectResponse
+     * @return RedirectResponse
      */
     public function update(Request $request, Invitation $invitation)
     {
@@ -127,7 +129,7 @@ class InvitationController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Invitation  $invitation
-     * @return \Illuminate\Http\RedirectResponse
+     * @return RedirectResponse
      */
     public function destroy(Invitation $invitation)
     {

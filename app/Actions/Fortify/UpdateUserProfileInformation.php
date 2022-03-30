56247f3204b55e211ddb4cc4495a1f26f2d6bd4a
Muelle96 <janni.mueller@yahoo.de>
2022-03-30 16:05:32 +0200

    fixed backend bugs with name/firstname/lastname

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -20,7 +20,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     {
 
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
             'phone_number' => ['string', 'max:15'],
@@ -38,7 +39,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $this->updateVerifiedUser($user, $input);
         } else {
             $user->forceFill([
-                'name' => $input['name'],
+                'first_name' => $input['first_name'],
+                'last_name' => $input['last_name'],
                 'email' => $input['email'],
                 'phone_number' => $input['phone_number'],
                 'position' => $input['position'],
@@ -58,7 +60,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     protected function updateVerifiedUser($user, array $input)
     {
         $user->forceFill([
-            'name' => $input['name'],
+            'first_name' => $input['first_name'],
+            'last_name' => $input['last_name'],
             'email' => $input['email'],
             'email_verified_at' => null,
             'phone_number' => $input['phone_number'],

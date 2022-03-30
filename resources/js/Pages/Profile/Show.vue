401a76a5bb5c769b0814ea465b9613e930f42f0d
StubyProjects <jakob.stuby@web.de>
2022-03-30 10:24:55 +0200

    Added flowbite ... take it easyy https://www.youtube.com/watch?v=GcCNcgoyG_0

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -7,6 +7,12 @@
                         <div class="space-y-8 divide-y divide-gray-200">
                             <div>
                                 <!-- Profile Photo -->
+                                <button data-tooltip-target="tooltip-default" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Default tooltip</button>
+                                <div id="tooltip-default" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+                                    Tooltip content
+                                    <div class="tooltip-arrow" data-popper-arrow></div>
+                                </div>
+
                                 <div class="col-span-6 sm:col-span-4">
                                     <!-- Profile Photo File Input -->
                                     <input type="file" class="hidden"
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import 'flowbite';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 

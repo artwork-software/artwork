d63639a763ab159a2ce174552af75915e49088ef
PaulDiTi <st161607@stud.uni-stuttgart.de>
2022-09-02 13:48:21 +0200

    fixed user seeder profile picture

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,7 +7,9 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class UserSeeder extends Seeder
 {
@@ -18,6 +20,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        Storage::put('/public/profile-photos/photo-1499996860823-5214fcc65f8f.jpg',
+            File::get(public_path('/profile-photos/photo-1499996860823-5214fcc65f8f.jpg')), 'public');
+        $this->command->info("Profile Photo set");
+
         $user = User::create([
             'first_name' => 'Max',
             'last_name' => 'Mustermann',

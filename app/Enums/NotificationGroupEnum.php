cf211f02340fea3dbc63b5d40c21d0f8bf8f2e10
StubyProjects <jakob.stuby@kanzleidrive.de>
2022-11-12 10:33:57 +0100

    Changed notification Settings

---
+++ app/Enums/NotificationGroupEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+class NotificationGroupEnum
+{
+    public const EVENTS = 'EVENTS';
+    public const ROOMS = 'ROOMS';
+    public const TASKS = 'TASKS';
+    public const PROJECTS = 'PROJECTS';
+
+}
---
+++ database/migrations/2022_11_12_095744_create_user_notification_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_notification_settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string("group_type");
+            $table->string("type");
+            $table->string("frequency")->default("daily");
+            $table->boolean("enabled_email")->default(true);
+            $table->boolean("enabled_push")->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_notification_settings');
+    }
+};

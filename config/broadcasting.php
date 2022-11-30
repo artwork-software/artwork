1083c7e2a16934704dbb837031b8c45d1d6e7826
Venom <timodeeken@web.de>
2022-11-30 15:38:50 +0100

    Websocket Port

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => false,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http'
+                'host' => '168.119.162.197',
+                'port' => 3341,
+                'scheme' => 'https'
             ],
         ],
 

6c1ee5f526de6690426eb511c81d046e6bc46e02
Paul Dietrich <paul.dietrich@tum.de>
2022-12-02 14:13:27 +0100

    minor change

--- database/factories/ContractFactory.php
+++ database/factories/ContractFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contract>
+ * @extends Factory
  */
 class ContractFactory extends Factory
 {

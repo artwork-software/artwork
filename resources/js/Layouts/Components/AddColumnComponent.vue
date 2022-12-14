cec14e4ebfa58f71c984fbf87683398cc3d9ca67
Muelle96 <janni.mueller@yahoo.de>
2022-12-14 16:49:43 +0100

    beginning of budgetComponentFrontend + start Add Column Modal

---
+++ resources/js/Layouts/Components/AddColumnComponent.vue
@@ -0,0 +1,62 @@
+<template>
+    <jet-dialog-modal :show="true" @close="closeModal()">
+        <template #content>
+            <img alt="Neue Spalte" src="/Svgs/Overlays/illu_budget_edit.svg" class="-ml-6 -mt-8 mb-4"/>
+            <XIcon @click="closeModal()" class="text-secondary h-5 w-5 right-0 top-0 mt-8 mr-5 absolute cursor-pointer"
+                   aria-hidden="true"/>
+            <div class="mx-4">
+                <!--   Heading   -->
+                <div>
+                    <h1 class="my-1 flex">
+                        <div class="flex-grow headline1">
+                            Neue Spalte
+                        </div>
+                    </h1>
+                    <h2 class="xsLight mb-2 mt-8">
+                        Lege eine neue, leere Spalte an. Alternativ kannst du auch eine Funktionsspalte (Summe/Differenz) anlegen.
+                    </h2>
+                </div>
+            </div>
+        </template>
+    </jet-dialog-modal>
+
+</template>
+
+<script>
+
+import JetDialogModal from "@/Jetstream/DialogModal";
+import {XIcon} from '@heroicons/vue/outline';
+
+export default {
+    name: 'AddColumnComponent',
+
+    components: {
+        JetDialogModal,
+        XIcon
+    },
+
+    data() {
+        return {
+
+        }
+    },
+
+    props: [],
+
+    emits: ['closed'],
+
+    watch: {
+    },
+
+    methods: {
+        openModal() {
+        },
+
+        closeModal(bool) {
+            this.$emit('closed', bool);
+        },
+    },
+}
+</script>
+
+<style scoped></style>
--- resources/js/Layouts/Components/BudgetComponent.vue
+++ resources/js/Layouts/Components/BudgetComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="mx-4">
 
+        <div class="w-full flex">
         <table class="w-full flex ml-16">
             <thead>
                 <tr>
@@ -8,6 +9,8 @@
                 </tr>
             </thead>
         </table>
+        <AddButton @click="openAddColumnModal()" class="w-44" text="Neue Spalte" mode="page"></AddButton>
+        </div>
 
         <div class="flex my-8 ">
             <div class="flex w-full bg-secondaryHover border border-2 border-gray-300">
@@ -21,10 +24,10 @@
 
                 <div class="bg-secondaryHover ml-8 w-full" v-if="costsOpened">
                     <div class="headline4 my-10">Ausgaben</div>
-                    <table class="w-full">
-                        <tbody>
-                            <tr v-for="mainPosition in tablesToShow[0]">
-                                <th class="bg-primary text-left">
+                    <table class="w-11/12 mb-6" >
+                        <tbody class="">
+                            <tr class="" v-for="(mainPosition,mainIndex) in tablesToShow[0]">
+                                <th class="bg-primary text-left p-0">
                                     <div class="pl-2 xsWhiteBold flex items-center h-10">
                                         {{ mainPosition.name }}
                                         <button class="my-auto w-6 ml-3" @click="mainPosition.closed = !mainPosition.closed">
@@ -32,11 +35,18 @@
                                             <ChevronDownIcon v-else class="h-6 w-6 text-white my-auto"></ChevronDownIcon>
                                         </button>
                                     </div>
-                                    <div class="bg-secondaryHover h-1"/>
-                                    <table v-if="!mainPosition.closed" class="w-full">
-                                        <thead>
-                                            <tr v-for="subPosition in mainPosition.sub_positions">
-                                                <th class="bg-silverGray xxsDark">
+                                    <!-- HIER ADD UNTERPOSITION -->
+                                    <div class="bg-secondaryHover h-1 flex justify-end border-dashed hover:border-t-2 hover:border-buttonBlue " @mouseover="hoveredBorder = mainIndex + 'subBefore'"
+                                         @mouseout="hoveredBorder = null">
+                                        <div v-if="hoveredBorder === mainIndex + 'subBefore'" class="uppercase text-secondaryHover text-sm -mt-8">
+                                            Unterposition
+                                            <PlusCircleIcon v-if="hoveredBorder === mainIndex + 'subBefore'" class="h-6 w-6 ml-12 text-secondaryHover bg-buttonBlue rounded-full" ></PlusCircleIcon>
+                                        </div>
+                                    </div>
+                                    <table v-if="!mainPosition.closed" class="w-full ">
+                                        <thead class="">
+                                            <tr class="" v-for="subPosition in mainPosition.sub_positions">
+                                                <th class="bg-silverGray xxsDark ">
                                                     <div class="pl-2 flex items-center h-10">
                                                         {{ subPosition.name }}
                                                         <button class="my-auto w-6 ml-3" @click="subPosition.closed = !subPosition.closed">
@@ -44,31 +54,68 @@
                                                             <ChevronDownIcon v-else class="h-6 w-6 text-primary my-auto"></ChevronDownIcon>
                                                         </button>
                                                     </div>
-                                                    <div class="bg-secondaryHover h-1"/>
-                                                    <table class="w-full flex" v-if="!subPosition.closed">
-                                                        <tbody>
-                                                            <tr class="bg-secondaryHover" v-for="row in subPosition.sub_position_rows">
+                                                    <table class="w-full" v-if="!subPosition.closed">
+                                                        <tbody class="bg-secondaryHover w-full">
+                                                            <tr class="bg-secondaryHover flex" v-for="row in subPosition.sub_position_rows">
                                                                 <td :class="index <= 1 ? 'w-24' : index === 2 ? 'w-72' : 'w-48'" v-for="(column,index) in row.columns">
-                                                                    <div class="ml-2 my-4" @click="column.pivot.clicked = !column.pivot.clicked"  v-if="!column.pivot.clicked">{{ column.pivot.value }}</div>
+                                                                    <div class="ml-2 my-4 xsDark" @click="column.pivot.clicked = !column.pivot.clicked"  v-if="!column.pivot.clicked">{{ column.pivot.value }}</div>
                                                                     <div v-else>
-                                                                        <input type="text" v-model="column.pivot.value" @focusout="column.pivot.clicked = !column.pivot.clicked">
+                                                                        <input class="xsDark my-1" type="text" v-model="column.pivot.value" @focusout="column.pivot.clicked = !column.pivot.clicked">
                                                                     </div>
                                                                 </td>
                                                             </tr>
-                                                            <tr class="bg-silverGray xsDark h-10">
-                                                                <td></td>
-                                                                <td></td>
-                                                                <td>SUM</td>
-                                                                <td>3000</td>
+                                                            <tr class="bg-silverGray xsDark flex h-10 w-full">
+                                                                <td class="w-24"></td>
+                                                                <td class="w-24"></td>
+                                                                <td class="w-72 ml-2 my-2">SUM</td>
+                                                                <td class="w-48 my-2">3000</td>
                                                             </tr>
                                                         </tbody>
                                                     </table>
                                                 </th>
                                             </tr>
+                                            <!-- HIER ADD UNTERPOSITION -->
+                                            <div class="bg-secondaryHover h-1 flex justify-end border-dashed hover:border-t-2 hover:border-buttonBlue " @mouseover="hoveredBorder = mainIndex + 'subAfter'"
+                                                 @mouseout="hoveredBorder = null">
+                                                <div v-if="hoveredBorder === mainIndex + 'subAfter'" class="uppercase text-buttonBlue text-sm -mt-8">
+                                                    Unterposition
+                                                    <PlusCircleIcon v-if="hoveredBorder === mainIndex + 'subAfter'" class="h-6 w-6 ml-12 text-secondaryHover bg-buttonBlue rounded-full" ></PlusCircleIcon>
+                                                </div>
+
+                                            </div>
+
+                                            <tr class="bg-primary xsWhiteBold flex h-10 w-full">
+                                                <td class="w-24"></td>
+                                                <td class="w-24"></td>
+                                                <td class="w-72 ml-2 my-2">SUM</td>
+                                                <td class="w-48 my-2">3002</td>
+                                            </tr>
                                         </thead>
+                                        <!-- HIER ADD HAUPTPOSITION -->
+                                        <div class="bg-secondaryHover h-1 flex justify-end border-dashed hover:border-t-2 hover:border-buttonBlue " @mouseover="hoveredBorder = mainIndex + 'main'"
+                                             @mouseout="hoveredBorder = null">
+                                            <div v-if="hoveredBorder === mainIndex + 'main'" class="uppercase text-secondaryHover text-sm -mt-8">
+                                                Hauptposition
+                                                <PlusCircleIcon v-if="hoveredBorder === mainIndex + 'main'" class="h-6 w-6 ml-12 text-secondaryHover bg-buttonBlue rounded-full" ></PlusCircleIcon>
+                                            </div>
+
+                                        </div>
                                     </table>
 
                                 </th>
+                            </tr>
+                            <tr class="bg-secondaryHover xsDark flex h-10 w-full">
+                                <td class="w-24"></td>
+                                <td class="w-24"></td>
+                                <td class="w-72 ml-2 my-2">SUM</td>
+                                <td class="w-48 my-2">3000</td>
+                            </tr>
+                            <!-- TODO: Hier noch einfügen if(commented === true) -->
+                            <tr v-if="true" class="bg-secondaryHover xsLight flex h-10 w-full">
+                                <td class="w-24"></td>
+                                <td class="w-24"></td>
+                                <td class="w-72 ml-2 my-2">SUM ausgeklammerte Posten</td>
+                                <td class="w-48 my-2">3000</td>
                             </tr>
                         </tbody>
                     </table>
@@ -155,27 +202,39 @@
             {{tablesToShow[0]}}
 
         </pre>
+    <!-- Termin erstellen Modal-->
+    <add-column-component
+        v-if="showAddColumnModal"
+        @closed="closeAddColumnModal()"
+    />
 
 </template>
 
 <script>
 
 
-import {ChevronDownIcon} from '@heroicons/vue/outline';
+import {ChevronDownIcon, PlusCircleIcon} from '@heroicons/vue/outline';
 import {ChevronUpIcon} from "@heroicons/vue/solid";
+import AddButton from "@/Layouts/Components/AddButton.vue";
+import AddColumnComponent from "@/Layouts/Components/AddColumnComponent.vue";
 
 export default {
     name: 'BudgetComponent',
 
     components: {
+        AddColumnComponent,
+        AddButton,
         ChevronDownIcon,
         ChevronUpIcon,
+        PlusCircleIcon
     },
 
     data() {
         return {
             costsOpened: true,
-            earningsOpened: true
+            earningsOpened: true,
+            hoveredBorder: null,
+            showAddColumnModal: false,
         }
     },
 
@@ -197,9 +256,15 @@ export default {
     },
 
     methods: {
-
+        openAddColumnModal(){
+            this.showAddColumnModal = true;
+        },
+        closeAddColumnModal(){
+            this.showAddColumnModal = false;
+        }
     },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>

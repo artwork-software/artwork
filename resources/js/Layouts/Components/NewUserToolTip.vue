40225919c99b878a01ad8a39f592b106b01a428f
Muelle96 <janni.mueller@yahoo.de>
2023-02-01 14:54:34 +0100

    implemented new Tooltip

--- resources/js/Layouts/Components/NewUserToolTip.vue
+++ resources/js/Layouts/Components/NewUserToolTip.vue
@@ -2,18 +2,18 @@
         <div class="flex flex-col items-start md:pl-0 md:items-center" :id="id">
             <div class="flex-col md:flex-row flex items-center md:justify-center">
                 <!--Code Block for white tooltip starts-->
-                <div class="relative mt-20 md:mt-0">
-                    <div class="mr-2 cursor-pointer">
-                        <img :src="user.profile_photo_url" alt="" class="mx-auto shrink-0 flex mt-2 object-cover rounded-full" :class="['h-' + this.height, 'w-' + this.width]" @mouseover="showTooltip(id)" @mouseout="hideTooltip(id)">
+                <div class="relative z-20 mt-20 md:mt-0">
+                    <div class="mr-1 cursor-pointer">
+                        <img :src="user.profile_photo_url" alt="" class="mx-auto shrink-0 flex object-cover rounded-full" :class="['h-' + this.height, 'w-' + this.width]" @mouseover="showTooltip(id)" @mouseout="hideTooltip(id)">
                     </div>
-                    <div :id="'tooltip' + id" role="tooltip" class="w-96 hidden z-20 -mt-40 -ml-6 absolute transition duration-150 ease-in-out left-0 ml-8 shadow-lg p-4 rounded text-secondary bg-primary">
+                    <div :id="'tooltip' + id" role="tooltip" class="w-96 hidden z-20 -mt-40 -ml-40 absolute transition duration-150 ease-in-out left-0 ml-8 shadow-lg p-4 rounded text-secondary bg-primary">
                         <div class="grid grid-cols-4 gap-x-2 flex items-center">
                             <div class="col-span-1 shrink-0 flex">
                                 <img class="mx-auto shrink-0 flex  h-14 w-14 mt-2 object-cover rounded-full"
                                      :src="user.profile_photo_url"
                                      alt=""/>
                             </div>
-                            <div class="col-span-3 ">
+                            <div class="col-span-3">
                                 <div class=" font-black font-lexend text-white text-lg">
                                     {{ user.first_name }} {{ user.last_name }}
                                 </div>

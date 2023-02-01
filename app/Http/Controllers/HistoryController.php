ca5209b08526cb461d41e59ca4cacd8f78904e50
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2023-02-01 09:50:00 +0100

    Fix Buffersize in History

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -15,7 +15,20 @@ class HistoryController extends Controller
 
     public function createHistory(int $modelId, string $historyText, string $type = 'project'): void
     {
-        $array[] = ['type' => $type, 'message' => $historyText, 'changed_by' => Auth::user()];
+        $user = Auth::user();
+        $userObj = [
+            'id' => $user->id,
+            'email' => $user->email,
+            'business' => $user->business,
+            'position' => $user->position,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'profile_photo_url' => $user->profile_photo_url,
+            'profile_photo_path' => $user->profile_photo_path,
+            'phone_number' => $user->phone_number,
+            'description' => $user->description
+        ];
+        $array[] = ['type' => $type, 'message' => $historyText, 'changed_by' => $userObj];
         Change::create([
             'model_id' => $modelId,
             'model_type' => $this->modelObject,

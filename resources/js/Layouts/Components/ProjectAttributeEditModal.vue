8f8b566d7f3b25e526522dabdb25ec778f7994b5
Muelle96 <janni.mueller@yahoo.de>
2023-03-08 11:24:09 +0100

    refactored project edit modal and update function of projectController

---
+++ resources/js/Layouts/Components/ProjectAttributeEditModal.vue
@@ -0,0 +1,115 @@
+<template>
+    <jet-dialog-modal :show="show" @close="closeModal">
+        <template #content>
+            <img src="/Svgs/Overlays/illu_project_edit.svg" class="-ml-6 -mt-8 mb-4" alt="artwork"/>
+            <div class="mx-4">
+                <div class="headline1 my-2">
+                    Projekteigenschaften
+                </div>
+                <XIcon @click="closeModal"
+                       class="h-5 w-5 right-0 top-0 mr-5 mt-8 flex text-secondary absolute cursor-pointer"
+                       aria-hidden="true"/>
+                <ProjectAttributesMenu
+                    :categories="traits.categories"
+                    :genres="traits.genres"
+                    :sectors="traits.sectors"
+                    :projectCategories="projectCategories"
+                    :projectGenres="projectGenres"
+                    :projectSectors="projectSectors"
+                    @update-project-category="updateProjectCategories"
+                    @update-project-sector="updateProjectSectors"
+                    @update-project-genre="updateProjectGenres"
+                />
+                <div class="mt-2 flex flex-wrap">
+                    <BaseFilterTag v-for="category in projectCategories" :filter="category.name" @remove-filter="updateProjectCategories(category)" class="w-fit" />
+                    <BaseFilterTag v-for="genre in projectGenres" :filter="genre.name" @remove-filter="updateProjectGenres(genre)" class="w-fit" />
+                    <BaseFilterTag v-for="sector in projectSectors" :filter="sector.name" @remove-filter="updateProjectSectors(sector)" class="w-fit" />
+                </div>
+            </div>
+            <div class="justify-center flex w-full my-6">
+                <AddButton text="Speichern" mode="modal" class="px-6 py-3" :disabled="name.length < 1"
+                           @click="updateProjectData"/>
+            </div>
+        </template>
+    </jet-dialog-modal>
+</template>
+
+<script>
+import JetDialogModal from "@/Jetstream/DialogModal";
+import JetInputError from "@/Jetstream/DialogModal";
+import AddButton from "@/Layouts/Components/AddButton";
+import {DownloadIcon, XIcon, ChevronDownIcon} from "@heroicons/vue/outline";
+import ProjectAttributesMenu from "@/Layouts/Components/ProjectAttributesMenu";
+import BaseFilterTag from "@/Layouts/Components/BaseFilterTag";
+
+export default {
+    name: "ProjectDataEditModal",
+    props: {
+        show: Boolean,
+        closeModal: Function,
+        project: Object,
+        traits: Object
+    },
+    components: {
+        BaseFilterTag,
+        ProjectAttributesMenu,
+        JetDialogModal,
+        JetInputError,
+        AddButton,
+        XIcon,
+        DownloadIcon,
+        ChevronDownIcon
+    },
+    data() {
+        return {
+            projectCategories: [],
+            projectGenres: [],
+            projectSectors: []
+        }
+    },
+    methods: {
+        updateProjectData() {
+            this.$inertia.patch(`/projects/${this.project.id}/attributes`, {
+                assignedSectorIds: this.createIdArray(this.projectSectors),
+                assignedCategoryIds: this.createIdArray(this.projectCategories),
+                assignedGenreIds: this.createIdArray(this.projectGenres),
+            }, {
+                preserveState: true,
+                preserveScroll: true
+            })
+        },
+        updateProjectCategories(category) {
+            this.projectCategories = this.updateTrait(category, this.projectCategories)
+        },
+        updateProjectGenres(genre) {
+            this.projectGenres = this.updateTrait(genre, this.projectGenres)
+        },
+        updateProjectSectors(sector) {
+           this.projectSectors = this.updateTrait(sector, this.projectSectors)
+        },
+        updateTrait(trait, array) {
+            if (typeof trait == "string") {
+                trait = array.find(sect => sect.name === trait)
+            }
+            if(!array.includes(trait)) {
+                trait.included = true
+                array.push(trait)
+            }
+            else {
+                trait.included = false
+                array = array.filter(item => item.id !== trait.id)
+            }
+            return array
+        },
+        createIdArray(array) {
+            let idArray = []
+            array.forEach(item => idArray.push(item.id))
+            return idArray
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

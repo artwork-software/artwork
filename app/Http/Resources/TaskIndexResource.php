559bd6c60ad789be827fcd4ada68ccd48837a4d0
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2023-03-14 10:28:42 +0100

    add user to task

--- app/Http/Resources/TaskIndexResource.php
+++ app/Http/Resources/TaskIndexResource.php
@@ -35,7 +35,8 @@ class TaskIndexResource extends JsonResource
             'done' => $this->done,
             'done_by_user' => $this->user_who_done,
             'done_at' => $this->done_at?->format('d.m.Y, H:i'),
-            'done_at_dt_local' => $this->done_at?->toDateTimeLocalString()
+            'done_at_dt_local' => $this->done_at?->toDateTimeLocalString(),
+            'users' => $this->task_users()->get()
         ];
     }
 }

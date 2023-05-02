3a6c0ebe426f70e5175b46035fa1875b676fc872
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2023-05-02 14:41:01 +0200

    fix Tooltip

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import VueTailwindDatepicker from 'vue-tailwind-datepicker'
-import 'flowbite';
 import '@vuepic/vue-datepicker/dist/main.css'
 
 const svgColors = {

7df87e4f70493cdf3a1afb44d8fde1b69f8913f9
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2023-12-06 12:33:10 +0100

    fix pdf creation bug

--- app/Http/Controllers/ExportPDFController.php
+++ app/Http/Controllers/ExportPDFController.php
@@ -2,30 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\EventTypPdfResource;
 use App\Http\Resources\ResourceModels\CalendarEventCollectionResourceModel;
 use App\Http\Resources\RoomPdfResource;
-use App\Models\EventType;
-use App\Models\Filter;
 use App\Models\Project;
 use App\Models\Room;
-
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
-
+use Symfony\Component\HttpFoundation\Response;
 
 class ExportPDFController extends Controller
 {
-
-
-    public function createPDF(Request $request, CalendarController $calendar){
-        if(!empty($request->project)){
+    public function createPDF(Request $request, CalendarController $calendar): Response
+    {
+        if (!empty($request->project)) {
             $project = Project::find($request->project);
         } else {
             $project = null;
@@ -42,7 +35,7 @@ class ExportPDFController extends Controller
             'rooms' => $calendar->filterRooms($request->input('start'), $request->input('end'))->get(),
             'filterRooms' => RoomPdfResource::collection(Room::all()),
             'calendar' => $showCalendar['roomsWithEvents'],
-            'dateValue'=>$showCalendar['dateValue'],
+            'dateValue' => $showCalendar['dateValue'],
             'days' => $showCalendar['days'],
             'selectedDate' => $showCalendar['selectedDate'],
             'filterOptions' => $showCalendar["filterOptions"],
@@ -58,8 +51,20 @@ class ExportPDFController extends Controller
                 events: new Collection(),
                 filter: new Collection(),
             ),
-        ])->setPaper($request->input('paperSize'), $request->input('paperOrientation'))->setOptions(['dpi' => $request->input('dpi'), 'defaultFont' => 'sans-serif']);
-        $pdfName = Carbon::now()->format('Y-m-d_H-i-s') . '_' . $request->input('paperOrientation') . '_'. str_replace(' ', '_', $request->title ) .'_dpi' . $request->input('dpi') . '.pdf';
+        ])
+            ->setPaper($request->input('paperSize'), $request->input('paperOrientation'))
+            ->setOptions(['dpi' => $request->input('dpi'), 'defaultFont' => 'sans-serif']);
+
+
+        $pdfName = Carbon::now()
+                ->format('Y-m-d_H-i-s') . '_' . $request
+                ->input('paperOrientation') . '_' . str_replace(' ', '_', $request->title) . '_dpi' . $request
+                ->input('dpi') . '.pdf';
+
+        if (!Storage::exists('pdf')) {
+            Storage::makeDirectory('pdf');
+        }
+
         $pdf->save(storage_path('app/pdf/' . $pdfName));
 
         // Rückgabe des Pfads zur heruntergeladenen Datei statt direktem Download
@@ -68,18 +73,12 @@ class ExportPDFController extends Controller
         return Inertia::location(\route('calendar.export.pdf.download', ['filename' => $pdfName]));
     }
 
-    public function download($filename){
-        return Storage::download('pdf/'. $filename, $filename, [
+    public function download($filename): \Symfony\Component\HttpFoundation\StreamedResponse
+    {
+        return Storage::download('pdf/' . $filename, $filename, [
             'Content-Type' => 'application/pdf',
-            'Content-Disposition' => 'attachment; filename="'.$filename.'"',
+            'Content-Disposition' => 'attachment; filename="' . $filename . '"',
             'Cache-Control' => 'no-cache, no-store, must-revalidate',
         ]);
     }
-
-
-    private function deleteFileAfterDownload($filename): void
-    {
-        Storage::delete('pdf/'. $filename);
-    }
-
 }

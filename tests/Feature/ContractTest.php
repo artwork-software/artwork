f56fb7b63bbd0ed608338fa22085a7ef8057df8d
Muelle96 <janni.mueller@yahoo.de>
2023-12-11 13:30:40 +0100

    finally fixed test

--- tests/Feature/ContractTest.php
+++ tests/Feature/ContractTest.php
@@ -5,13 +5,13 @@ use App\Models\Project;
 use App\Models\User;
 use Illuminate\Http\UploadedFile;
 
-beforeEach(function() {
+beforeEach(function (): void {
     $this->user = User::factory()->create();
     $this->project = Project::factory()->create();
     $this->contract = Contract::factory()->create();
 });
 
-test('contracts can be created with the correct variables', function () {
+test('contracts can be created with the correct variables', function (): void {
 
     $this->user->assignRole(\App\Enums\RoleNameEnum::ARTWORK_ADMIN->value);
 
@@ -24,40 +24,38 @@ test('contracts can be created with the correct variables', function () {
         'description' => 'Test description',
         'ksk_liable' => true,
         'resident_abroad' => true,
-    ]);
+     ]);
 
     $this->assertDatabaseHas('contracts', [
         'name' => 'document.pdf',
         'project_id' => $this->project->id
     ]);
-
 });
 
-test('contracts can be updated', function() {
+test('contracts can be updated', function (): void {
 
-   $this->user->assignRole(\App\Enums\RoleNameEnum::ARTWORK_ADMIN->value);
+    $this->user->assignRole(\App\Enums\RoleNameEnum::ARTWORK_ADMIN->value);
     $this->project->contracts()->save($this->contract);
 
     $this->actingAs($this->user);
 
-   $this->patch("/contracts/{$this->contract->id}", [
+    $this->patch("/contracts/{$this->contract->id}", [
         'contract' => UploadedFile::fake()->create('document2.pdf', 100),
         'contract_partner' => 'Agentur Hamburg',
         'amount' => 2000,
         'description' => 'Test description',
-   ]);
+    ]);
    //assures that only the fields that were updated changed, and that the rest stays the same.
-   $this->assertDatabaseHas('contracts', [
-        'contract' => 'document2.pdf',
+    $this->assertDatabaseHas('contracts', [
+        'name' => 'document2.pdf',
         'project_id' => $this->project->id,
         'contract_partner' => 'Agentur Hamburg',
         'amount' => 2000,
        'description' => 'Test description',
     ]);
-
 });
 
-test('contracts can be deleted', function() {
+test('contracts can be deleted', function (): void {
 
     $this->user->assignRole(\App\Enums\RoleNameEnum::ARTWORK_ADMIN->value);
     $this->project->contracts()->save($this->contract);
@@ -70,5 +68,4 @@ test('contracts can be deleted', function() {
     $this->assertDatabaseMissing('contracts', [
         'id' => $this->contract->id,
     ]);
-
 });

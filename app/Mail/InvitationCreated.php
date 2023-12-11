80088c3c798c583f7b99c5a646982d73f59ab307
Jonas Glauß <glauss-jonas@web.de>
2023-12-11 16:08:02 +0100

    phpcs fixes, remove docblocks

--- app/Mail/InvitationCreated.php
+++ app/Mail/InvitationCreated.php
@@ -5,22 +5,20 @@ namespace App\Mail;
 use App\Models\Invitation;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
 class InvitationCreated extends Mailable
 {
-    use Queueable, SerializesModels;
+    use Queueable;
+    use SerializesModels;
 
     public $invitation;
+
     public $user;
+
     public $token;
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
+
     public function __construct(Invitation $invitation, Authenticatable $user, $token)
     {
         $this->invitation = $invitation;
@@ -28,12 +26,7 @@ class InvitationCreated extends Mailable
         $this->token = $token;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
+    public function build(): InvitationCreated
     {
         return $this->from("einladung@test.de", $this->user->first_name)
             ->replyTo($this->user->email)
--- app/Policies/GenrePolicy.php
+++ app/Policies/GenrePolicy.php
@@ -11,73 +11,31 @@ class GenrePolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
+    public function viewAny(User $user): bool
     {
         return $user->can(PermissionNameEnum::PROJECT_SETTINGS_UPDATE->value);
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
+    public function create(User $user): bool
     {
         return $user->can(PermissionNameEnum::PROJECT_SETTINGS_UPDATE->value);
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Genre $genre)
+    public function update(User $user): bool
     {
         return $user->can(PermissionNameEnum::PROJECT_SETTINGS_UPDATE->value);
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Genre $genre)
+    public function delete(User $user): bool
     {
         return $user->can(PermissionNameEnum::PROJECT_SETTINGS_UPDATE->value);
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Genre $genre)
+    public function restore(): void
     {
-        //
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Genre  $genre
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Genre $genre)
+    public function forceDelete(User $user, Genre $genre): void
     {
-        //
     }
 }

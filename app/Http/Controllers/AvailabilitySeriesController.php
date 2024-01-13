e8c5b4df25f98929609f5a140541ef25bcb4eb02
jason roeser <jason@updated-data.me>
2024-01-13 20:32:53 +0100

    Fix phpstan

--- app/Http/Controllers/AvailabilitySeriesController.php
+++ app/Http/Controllers/AvailabilitySeriesController.php
@@ -7,78 +7,37 @@ use Illuminate\Http\Request;
 
 class AvailabilitySeriesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(): \Illuminate\Http\Response
+    public function index(): void
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(): \Illuminate\Http\Response
+    public function create(): void
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request): \Illuminate\Http\Response
+    public function store(Request $request): void
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \Artwork\Modules\Availability\Models\AvailabilitySeries $availabilitySeries
-     * @return \Illuminate\Http\Response
-     */
-    public function show(AvailabilitySeries $availabilitySeries): \Illuminate\Http\Response
+    public function show(AvailabilitySeries $availabilitySeries): void
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \Artwork\Modules\Availability\Models\AvailabilitySeries $availabilitySeries
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(AvailabilitySeries $availabilitySeries): \Illuminate\Http\Response
+    public function edit(AvailabilitySeries $availabilitySeries): void
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Artwork\Modules\Availability\Models\AvailabilitySeries $availabilitySeries
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, AvailabilitySeries $availabilitySeries): \Illuminate\Http\Response
+    public function update(Request $request, AvailabilitySeries $availabilitySeries): void
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \Artwork\Modules\Availability\Models\AvailabilitySeries $availabilitySeries
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(AvailabilitySeries $availabilitySeries): \Illuminate\Http\Response
+    public function destroy(AvailabilitySeries $availabilitySeries): void
     {
         //
     }

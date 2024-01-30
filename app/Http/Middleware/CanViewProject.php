ff603f79019c665f91ae3bc2402d8321a3ea2ca2
Muelle96 <janni.mueller@yahoo.de>
2024-01-30 14:36:39 +0100

    fixed bugs

--- app/Http/Middleware/CanViewProject.php
+++ app/Http/Middleware/CanViewProject.php
@@ -17,13 +17,14 @@ class CanViewProject
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
     {
         $project = $request->route('project');
 
         if (
             Auth::user()->hasRole(RoleNameEnum::ARTWORK_ADMIN->value)
-            || ((Auth::user()->hasPermissionTo(PermissionNameEnum::PROJECT_VIEW->value) || Auth::user()->hasPermissionTo(PermissionNameEnum::PROJECT_UPDATE->value))
+            || ((Auth::user()->hasPermissionTo(PermissionNameEnum::PROJECT_VIEW->value)
+                    || Auth::user()->hasPermissionTo(PermissionNameEnum::PROJECT_UPDATE->value))
                 && $project->users()->where('users.id', Auth::id())->first())
         ) {
             return $next($request);

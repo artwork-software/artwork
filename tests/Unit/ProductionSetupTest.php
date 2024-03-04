cf4c14a75f43bcb0bd7246bc8099d012e0847372
jason roeser <jason@updated-data.me>
2024-03-04 10:50:53 +0100

    Satisfy phpcs

--- tests/Unit/ProductionSetupTest.php
+++ tests/Unit/ProductionSetupTest.php
@@ -3,12 +3,14 @@
 namespace Tests\Unit;
 
 use Illuminate\Support\Facades\Artisan;
+
 use function Pest\Laravel\assertDatabaseMissing;
 use function Pest\Laravel\assertDatabaseHas;
 
-test('production seeder works', function () {
+test('production seeder works', function ():void {
 
     Artisan::call('migrate:fresh --force');
+
     assertDatabaseMissing('permission_presets', [
         'name' => 'Standard User'
     ]);

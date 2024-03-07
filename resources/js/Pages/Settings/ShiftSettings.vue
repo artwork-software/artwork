01532e4c98b92ebafd3615148a73a5962a83f34f
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2024-03-07 17:01:16 +0100

    fix z-index menu

--- resources/js/Pages/Settings/ShiftSettings.vue
+++ resources/js/Pages/Settings/ShiftSettings.vue
@@ -53,7 +53,7 @@
                                         leave-from-class="transform opacity-100 scale-100"
                                         leave-to-class="transform opacity-0 scale-95">
                                 <MenuItems
-                                    class="cursor-pointer origin-top-left absolute left-0 mr-4 mt-2 w-72 shadow-lg bg-primary ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
+                                    class="cursor-pointer z-30 origin-top-left absolute left-0 mr-4 mt-2 w-72 shadow-lg bg-primary ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
                                     <div class="py-1">
                                         <MenuItem @click="updateCraft(craft)"
                                             v-slot="{ active }">

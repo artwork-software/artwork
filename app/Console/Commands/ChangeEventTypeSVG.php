1f9875cc76afd24abc353a1f8f9b9c053b8c7d2e
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2024-03-08 14:45:58 +0100

    add command

---
+++ app/Console/Commands/ChangeEventTypeSVG.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Database\Seeders\ChangeEventTypeSvgToHexSeed;
+use Illuminate\Console\Command;
+
+class ChangeEventTypeSVG extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:change-event-type';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Change event type svg to hex';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call(ChangeEventTypeSvgToHexSeed::class)
+    }
+}

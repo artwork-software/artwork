a025d5c09564320713f12b925eb4664c687bcd88
Jonas Glauß <glauss-jonas@web.de>
2024-04-04 11:14:04 +0200

    - add sage specific day import

--- app/Console/Commands/GetSage100Data.php
+++ app/Console/Commands/GetSage100Data.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 
 class GetSage100Data extends Command
 {
-    protected $signature = 'app:get-sage100-data {count?}';
+    protected $signature = 'app:get-sage100-data {count?} {specificDay?}';
 
     protected $description = 'Get data from Sage100 and import it to budget.';
 
@@ -19,6 +19,9 @@ class GetSage100Data extends Command
 
     public function handle(): int
     {
-        return $this->sage100Service->importDataToBudget($this->argument('count'));
+        return $this->sage100Service->importDataToBudget(
+            $this->argument('count'),
+            $this->argument('specificDay')
+        );
     }
 }

f396ea8dded8755623b29fed85bdaefb89781c3f
Jonas Glauß <glauss-jonas@web.de>
2024-04-04 15:51:47 +0200

    - fix shift history

--- resources/js/Pages/Shifts/Components/ShiftHistoryModal.vue
+++ resources/js/Pages/Shifts/Components/ShiftHistoryModal.vue
@@ -12,34 +12,30 @@
                 <div class="text-secondary subpixel-antialiased">
                     {{$t('Here you can see what was changed by whom and when.')}}
                 </div>
-
-
                 <div class="flex w-full flex-wrap mt-4 max-h-96">
                     <div v-for="(historyItem , index) in history">
                         <div class="flex w-full my-1" v-if="historyItem?.changes !== null">
                             <div class="flex w-full ">
                                     <span class="w-40 text-secondary my-auto text-sm subpixel-antialiased">
-                                        {{ dayjs(historyItem.created_at).format('DD.MM.YYYY HH:mm') }}
+                                        {{ historyItem.created_at }}
                                     </span>
-                                <UserPopoverTooltip :height="7" :width="7" v-if="JSON.parse(historyItem.changes, null, 2)[0].changed_by"
-                                                :user="JSON.parse(historyItem.changes, null, 2)[0].changed_by" :id="index"/>
+                                <UserPopoverTooltip :height="7" :width="7" v-if="historyItem.changes[0].changed_by"
+                                                :user="historyItem.changes[0].changed_by" :id="index"/>
                                 <div v-else class="xsLight ml-3">
                                     {{ $t('deleted User')}}
                                 </div>
                                 <div class="text-secondary subpixel-antialiased ml-2 text-sm my-auto w-96">
-                                    <p>
-                                        {{ JSON.parse(historyItem.changes, null, 2)[0].event_title }}
-                                    </p>
-                                    <p>
-                                        {{ JSON.parse(historyItem.changes, null, 2)[0].message }}
-                                    </p>
+                                    {{
+                                        $t(
+                                            historyItem.changes[0].translationKey,
+                                            historyItem.changes[0].translationKeyPlaceholderValues
+                                        )
+                                    }}
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
-
             </div>
         </template>
     </jet-dialog-modal>

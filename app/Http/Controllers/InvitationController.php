fc278469f5a7a939f8a15d1c20fd75f81657707e
Jonas Glauß <glauss-jonas@web.de>
2024-04-25 10:39:44 +0200

    - some cleanups - fix invitation

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -16,6 +16,7 @@ use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;

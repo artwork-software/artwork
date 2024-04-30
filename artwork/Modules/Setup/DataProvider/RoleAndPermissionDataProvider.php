50a3734e1c62a1546e52d959ce252f2df9a8142c
Jonas Glauß <glauss-jonas@web.de>
2024-04-30 16:48:37 +0200

    - revert changes to fix seeding

--- artwork/Modules/Setup/DataProvider/RoleAndPermissionDataProvider.php
+++ artwork/Modules/Setup/DataProvider/RoleAndPermissionDataProvider.php
@@ -4,23 +4,11 @@ namespace Artwork\Modules\Setup\DataProvider;
 
 interface RoleAndPermissionDataProvider
 {
-    /**
-     * @return array<mixed, mixed>
-     */
     public function getPermissions(): array;
 
-    /**
-     * @return array<mixed, mixed>
-     */
     public function getRoles(): array;
 
-    /**
-     * @return array<mixed, mixed>
-     */
     public function getExcludedPermissionColumns(): array;
 
-    /**
-     * @return array<mixed, mixed>
-     */
     public function getExcludedRoleColumns(): array;
 }

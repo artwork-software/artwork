372b28f53c47fcc50f0a6551a6b52374b03c59e3
JasonRoeser <hello@updated-data.me>
2024-05-07 03:43:03 +0200

    86byhzyn2 - reworked all the docker things

--- artwork/Core/Console/Commands/UpdateContainerCommand.php
+++ artwork/Core/Console/Commands/UpdateContainerCommand.php
@@ -5,22 +5,21 @@ namespace Artwork\Core\Console\Commands;
 use Artwork\Modules\Permission\Models\Permission;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class UpdateContainerCommand extends Command
 {
 
-    protected $signature = 'artwork:container-update';
+    protected $signature = 'container-update';
 
     protected $description = 'Updates the container';
 
     public function handle(): void
     {
-        if(!env('APP_KEY')) {
-            Artisan::call('key:generate --force');
-        }
-
+        DB::raw(sprintf('CREATE DATABASE IF NOT EXISTS `%s`', env('DB_DATABASE')));
         Artisan::call('migrate --force');
-
+        exec('npm run dev');
+        exec('npm run prod');
         if(!Permission::first()) {
             Artisan::call('db:seed:production');
         }

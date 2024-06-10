dad16460cadc49eb1ad3b9af395fde51c6a7643b
Jonas Glauß <glauss-jonas@web.de>
2024-06-10 09:49:15 +0200

    #refs ARTWORK-26-shift-time-axis | Feature
    - remove log

--- artwork/Modules/Event/Services/EventService.php
+++ artwork/Modules/Event/Services/EventService.php
@@ -854,7 +854,7 @@ readonly class EventService
             $shiftStart = Carbon::parse($shift->start)->format('H:i:s');
             $shiftStartDate = Carbon::parse($shift->start_date)->format('Y-m-d');
             $startDateTime = Carbon::parse($shiftStartDate . ' ' . $shiftStart);
-            Log::debug($startDateTime->format('d.m.Y H:i:s'));
+
             if ($startDateTime->isBefore($earliestStartTime)) {
                 $earliestStartTime = $startDateTime;
             }

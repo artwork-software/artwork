7cd3f164c8604e31a901ec4a14050cbb4e11160a
Jonas Glauß <glauss-jonas@web.de>
2024-06-13 13:39:12 +0200

    refs #ARTWORK-40-inventory-management | Add inventory management
    - fix webpack compiler warnings

--- resources/js/Pages/Shifts/Components/SingleEventInShiftPlan.vue
+++ resources/js/Pages/Shifts/Components/SingleEventInShiftPlan.vue
@@ -1,42 +1,8 @@
-<script setup>
-
-import { defineProps } from 'vue';
-import {Link} from "@inertiajs/inertia-vue3";
-import {IconLock, IconRepeat, IconUsersGroup} from "@tabler/icons-vue";
-import ShiftDropElement from "@/Layouts/Components/ShiftPlanComponents/ShiftDropElement.vue";
-
-// import backgroundColorWithOpacity from mixin '../../../Mixins/ColorHelper.vue'
-
-const backgroundColorWithOpacity = (color, percent = 15) => {
-    if (!color) return `rgb(255, 255, 255, ${percent}%)`;
-    return `rgb(${parseInt(color.slice(-6, -4), 16)}, ${parseInt(color.slice(-4, -2), 16)}, ${parseInt(color.slice(-2), 16)}, ${percent}%)`;
-};
-
-
-
-const TextColorWithDarken = (color, percent = 75) => {
-    if (!color) return 'rgb(180, 180, 180)';
-    return `rgb(${parseInt(color.slice(-6, -4), 16) - percent}, ${parseInt(color.slice(-4, -2), 16) - percent}, ${parseInt(color.slice(-2), 16) - percent})`;
-}
-
-const pageProps = defineProps({
-    event: {
-        type: Object,
-        required: true,
-    },
-    day: {
-        type: Object,
-        required: true,
-    },
-})
-
-</script>
-
 <template>
     <div>
         <div>
             <div class="text-secondaryHover xsWhiteBold px-1 py-1 rounded-lg"
-                 :style="{backgroundColor: backgroundColorWithOpacity(event.event_type?.hex_code), color: TextColorWithDarken(event.event_type?.hex_code)}">
+                 :style="{backgroundColor: backgroundColorWithOpacity(event.event_type?.hex_code), color: textColorWithDarken(event.event_type?.hex_code)}">
                 <div class="w-40 truncate">
                     {{ event.event_type.abbreviation }}: {{ event?.eventName ?? event?.project?.name }}
                 </div>
@@ -56,6 +22,24 @@ const pageProps = defineProps({
     </div>
 </template>
 
-<style scoped>
+<script setup>
+defineProps({
+    event: {
+        type: Object,
+        required: true,
+    },
+    day: {
+        type: Object,
+        required: true,
+    },
+});
 
-</style>
+const backgroundColorWithOpacity = (color, percent = 15) => {
+        if (!color) return `rgb(255, 255, 255, ${percent}%)`;
+        return `rgb(${parseInt(color.slice(-6, -4), 16)}, ${parseInt(color.slice(-4, -2), 16)}, ${parseInt(color.slice(-2), 16)}, ${percent}%)`;
+    },
+    textColorWithDarken = (color, percent = 75) => {
+        if (!color) return 'rgb(180, 180, 180)';
+        return `rgb(${parseInt(color.slice(-6, -4), 16) - percent}, ${parseInt(color.slice(-4, -2), 16) - percent}, ${parseInt(color.slice(-2), 16) - percent})`;
+    };
+</script>

c0bbeb54f1132d93392d59b286c3281a50899a04
Jonas Glauß <glauss-jonas@web.de>
2024-06-19 22:14:58 +0200

    refs #ARTWORK-40-inventory-management | Add inventory management
    - add add new column modal (ui) + validations

--- resources/js/Pages/Inventory/AddColumnModal.vue
+++ resources/js/Pages/Inventory/AddColumnModal.vue
@@ -2,23 +2,54 @@
     <BaseModal v-if="show" @closed="close">
         <div class="flex flex-col mx-4 gap-y-2">
             <h1 class="headline1">
-                Neue Spalte erstellen
+                {{ $t('Neue Spalte erstellen') }}
             </h1>
             <div id="new-column-form"
                  class="flex flex-col gap-y-2">
                 <TextInputComponent id="new-column-name"
                                     v-model="newColumnForm.name"
-                                    label="Spaltenname"
+                                    :label="$t('Spaltenname*')"
                 />
+                <span v-if="showNewColumnFormNameError"
+                      class="text-xs subpixel-antialiased text-error">
+                    {{ $t('Es muss ein Name angegeben werden.') }}
+                </span>
                 <SelectComponent id="new-column-type"
                                  v-model="newColumnForm.type"
-                                 label="Spaltentyp"
+                                 :label="$t('Spaltentyp')"
                                  :options="getTypeOptions()"
                                  :getter-for-options-to-display="(type) => type.value"
                                  selected-property-to-display="value"
                 />
-                <div v-if="newColumnForm.type?.id === NEW_COLUMN_TYPE.SELECT.id">
-                    Auswahlbox-Optionen:
+                <div v-if="newColumnForm.type?.id === NEW_COLUMN_TYPE.SELECT.id"
+                     class="w-full flex flex-col">
+                    <span class="text-xs subpixel-antialiased">
+                        {{ $t('Neue Auswahlmöglichkeit hinzufügen:') }}
+                    </span>
+                    <div class="flex flex-row w-1/2 items-center gap-x-2">
+                        <TextInputComponent id="new-select-option-name"
+                                            v-model="newColumnNewSelectOptionName"
+                                            :label="$t('Auswahlmöglichkeit')"/>
+                        <IconPlus v-if="newColumnNewSelectOptionName.length > 0"
+                                  class="w-6 h-6 cursor-pointer translate-y-2.5 p-0.5 subpixel-antialiased rounded-full text-white bg-black hover:bg-green-500"
+                                  @click="addNewColumnNewSelectOption()"/>
+                    </div>
+                    <span v-if="showNewColumnFormTypeOptionsError"
+                          class="text-xs subpixel-antialiased text-error mt-2">
+                        {{ $t('Es muss mindestens eine Auswahloption hinzugefügt werden.') }}
+                    </span>
+                    <div class="flex flex-col text-xs">
+                        <div v-for="(option, index) in newColumnForm.selectOptions"
+                            class="flex flex-row items-center gap-x-2 mt-2">
+                            <span>{{ option }}</span>
+                            <IconTrashXFilled class="w-6 h-6 cursor-pointer p-0.5 subpixel-antialiased rounded-full text-white bg-black hover:bg-red-500"
+                                              @click="removeNewColumnNewSelectOption(index)"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full flex flex-row justify-center mt-4">
+                    <FormButton :text="$t('Save')"
+                                @click="saveNewColumn()"/>
                 </div>
             </div>
         </div>
@@ -26,11 +57,13 @@
 </template>
 
 <script setup>
+import {IconPlus, IconTrashXFilled} from "@tabler/icons-vue";
 import BaseModal from "@/Components/Modals/BaseModal.vue";
 import {useForm} from "@inertiajs/inertia-vue3";
 import {ref} from "vue";
 import TextInputComponent from "@/Components/Inputs/TextInputComponent.vue";
 import SelectComponent from "@/Components/Inputs/SelectComponent.vue";
+import FormButton from "@/Layouts/Components/General/Buttons/FormButton.vue";
 
 const emits = defineEmits(['closed']);
 const NEW_COLUMN_TYPE = {
@@ -54,14 +87,16 @@ const NEW_COLUMN_TYPE = {
     props = defineProps({
         show: Boolean
     }),
-    showNewColumnFormNameError = ref(false),
-    newColumnFormNameErrorText = ref(''),
-    showNewColumnFormTypeOptionsError = ref(false),
-    newColumnFormTypeOptionsErrorText = ref(''),
+    newColumnNewSelectOptionName = ref(''),
     newColumnForm = useForm({
         name: null,
-        type: null
+        type: null,
+        selectOptions: []
     }),
+    showNewColumnFormNameError = ref(false),
+    showNewColumnFormTypeOptionsError = ref(false),
+    newColumnFormNameErrorText = ref(''),
+    newColumnFormTypeOptionsErrorText = ref(''),
     getTypeOptions = () => {
         return [
             {
@@ -82,8 +117,15 @@ const NEW_COLUMN_TYPE = {
             }
         ];
     },
+    addNewColumnNewSelectOption = () => {
+        newColumnForm.selectOptions.push(newColumnNewSelectOptionName.value);
+        newColumnNewSelectOptionName.value = '';
+    },
+    removeNewColumnNewSelectOption = (index) => {
+        newColumnForm.selectOptions.splice(index, 1);
+    },
     validateNewColumn = () => {
-        if (newColumnForm.name.length === 0) {
+        if (newColumnForm.name === null || newColumnForm.name.length === 0) {
             showNewColumnFormNameError.value = true;
             newColumnFormNameErrorText.value = 'Es muss ein Spaltenname angegeben werden.'
         } else {
@@ -91,21 +133,23 @@ const NEW_COLUMN_TYPE = {
             newColumnFormNameErrorText.value = '';
         }
 
-        // if (newColumnForm.typeOptions.length > 1) {
-        //     showNewColumnFormNameError.value = true;
-        //     newColumnFormNameErrorText.value = 'Es muss mindestens eine Auswahloption hinzugefügt werden.';
-        // } else {
-        //     showNewColumnFormNameError.value = false;
-        //     newColumnFormNameErrorText.value = '';
-        // }
-        //
-        // if (newColumnForm.type === 'select') {
-        //     if (newColumnForm.typeOptions.hasDefault) {
-        //         //validate if default is selected
-        //     }
-        // }
+        if (newColumnForm.type !== null && newColumnForm.type.id === NEW_COLUMN_TYPE.SELECT.id && newColumnForm.selectOptions.length < 1) {
+            showNewColumnFormTypeOptionsError.value = true;
+            newColumnFormTypeOptionsErrorText.value = 'Es muss mindestens eine Auswahloption hinzugefügt werden.';
+        } else {
+            showNewColumnFormTypeOptionsError.value = false;
+            newColumnFormTypeOptionsErrorText.value = '';
+        }
+
+        return !showNewColumnFormNameError.value && !showNewColumnFormTypeOptionsError.value;
     },
     saveNewColumn = () => {
+        if (!validateNewColumn()) {
+            return;
+        }
+
+        console.debug('validated', newColumnForm);
+
         //newColumnForm.post(route('#'));
     },
     close = () => {

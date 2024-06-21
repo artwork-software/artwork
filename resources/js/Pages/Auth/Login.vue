8d3f83ffa113cda16820b6befd6874965be52890
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2024-06-21 17:17:03 +0200

    fix phpcs errors

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,8 @@
 <template>
 
+    <Head>
+        <title>{{ $t('Login') }}</title>
+    </Head>
     <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
         <div class="flex flex-col items-center justify-center h-screen">
             <div class="min-h-96">
@@ -66,59 +69,6 @@
             <img class="h-screen w-full object-cover" :src="$page.props.banner" alt=""/>
         </div>
     </div>
-
-
-
-    <div class="min-h-full hidden">
-        <img :src="$page.props.big_logo" class="w-20 h-20 ml-12 mt-12 absolute rounded-full" />
-        <div
-            class="flex-1 flex min-h-screen flex-col align-items-center justify-center py-12 px-4 sm:px-6 lg:px-20 xl:px-24">
-            <div class="mx-auto  w-full max-w-sm lg:w-96">
-
-
-
-                <div class="mt-8">
-                    <div class="mt-6">
-                        <form class="space-y-6" @submit.prevent="submit">
-                            <div class="relative w-full mr-4">
-                                <input id="email" v-model="form.email" type="text" class="peer pl-0 h-12 w-full focus:border-t-transparent focus:border-primary focus:ring-0 border-l-0 border-t-0 border-r-0 border-b-2 border-gray-300 text-primary placeholder-secondary placeholder-transparent" placeholder="placeholder" />
-                                <label for="email" class="absolute left-0 text-sm -top-5 text-gray-600 text-sm -top-3.5 transition-all subpixel-antialiased focus:outline-none text-secondary peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-sm ">{{$t('Email')}}*</label>
-                            </div>
-
-                            <div class="relative w-full mr-4">
-                                <input id="password" v-model="form.password" type="password" class="peer pl-0 h-12 w-full focus:border-t-transparent focus:border-primary focus:ring-0 border-l-0 border-t-0 border-r-0 border-b-2 border-gray-300 text-primary placeholder-secondary placeholder-transparent" placeholder="placeholder" />
-                                <label for="password" class="absolute left-0 text-sm -top-5 text-gray-600 text-sm -top-3.5 transition-all subpixel-antialiased focus:outline-none text-secondary peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-sm ">{{ $t('Password')}}*</label>
-                            </div>
-                            <jet-input-error :message="errors.email" class="mt-2"/>
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center">
-                                    <Checkbox class="justify-between text-sm" :item=rememberCheckbox />
-                                </div>
-                                <div class="text-sm">
-                                    <Link v-if="canResetPassword" :href="route('password.request')"
-                                          class="text-xs text-secondary subpixel-antialiased hover:font-semibold hover:text-primary">
-                                        {{$t('Forgot your password?')}}
-                                    </Link>
-                                </div>
-                            </div>
-
-
-                            <div>
-                                <BaseButton :text="$t('Login')" :disabled="this.form.email === '' || this.form.password === ''" horizontal-padding="px-44" vertical-padding="py-4" type="submit" />
-                            </div>
-                        </form>
-
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <div class="hidden lg:block relative w-0 flex-1">
-            <img class="absolute inset-0 h-full w-full object-cover"
-                 :src="$page.props.banner"
-                 alt=""/>
-        </div>
-    </div>
 </template>
 
 <script>
@@ -153,7 +103,7 @@ export default defineComponent({
         JetLabel,
         JetValidationErrors,
         Link,
-        Checkbox,
+        Checkbox
     },
     props: {
         canResetPassword: Boolean,

39c96c4bfc475946e7d8d6968c371b147ad59de2
Jonas Glauß <glauss-jonas@web.de>
2024-07-01 14:54:19 +0200

    refs #ARTWORK-40-inventory-management
    - move edit button in menu for select types after coloring

--- resources/js/Pages/Inventory/InventoryManagement/Inventory.vue
+++ resources/js/Pages/Inventory/InventoryManagement/Inventory.vue
@@ -45,15 +45,6 @@
                                                 leave-from-class="transform opacity-100 scale-100"
                                                 leave-to-class="transform opacity-0 scale-95">
                                         <MenuItems class="absolute z-50 -translate-x-[110%] top-2 shadow-lg rounded-xl bg-artwork-navigation-background focus:outline-none">
-                                            <MenuItem v-if="column.type === 3"
-                                                      v-slot="{ active }"
-                                                      as="div">
-                                                <a @click="openEditColumnSelectOptionsModal(column)"
-                                                   :class="[active ? 'rounded-xl bg-artwork-navigation-color/10 text-white' : 'text-secondary', 'cursor-pointer group flex items-center px-4 py-2 subpixel-antialiased']">
-                                                    <IconEdit class="mr-3 h-5 w-5 text-primaryText group-hover:text-white"/>
-                                                    {{ $t('Edit') }}
-                                                </a>
-                                            </MenuItem>
                                             <MenuItem v-slot="{ active }" as="div">
                                                 <a @click="column.showColorMenu = true"
                                                    :class="[active ? 'rounded-xl bg-artwork-navigation-color/10 text-white' : 'text-secondary', 'cursor-pointer group flex items-center px-4 py-2 subpixel-antialiased']">
@@ -65,6 +56,15 @@
                                                               d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"/>
                                                     </svg>
                                                     {{ $t('Coloring') }}
+                                                </a>
+                                            </MenuItem>
+                                            <MenuItem v-if="column.type === 3"
+                                                      v-slot="{ active }"
+                                                      as="div">
+                                                <a @click="openEditColumnSelectOptionsModal(column)"
+                                                   :class="[active ? 'rounded-xl bg-artwork-navigation-color/10 text-white' : 'text-secondary', 'cursor-pointer group flex items-center px-4 py-2 subpixel-antialiased']">
+                                                    <IconEdit class="mr-3 h-5 w-5 text-primaryText group-hover:text-white"/>
+                                                    {{ $t('Edit') }}
                                                 </a>
                                             </MenuItem>
                                             <MenuItem v-slot="{ active }" as="div">

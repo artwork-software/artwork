dcdf37897002d4a7a7967cc7d288c5442b25cecb
Jonas Glauß <glauss-jonas@web.de>
2024-07-01 17:46:21 +0200

    refs #ARTWORK-40-inventory-management
    - scss for Inventory and InventoryTopBar

--- resources/js/Pages/Inventory/InventoryManagement/Inventory.vue
+++ resources/js/Pages/Inventory/InventoryManagement/Inventory.vue
@@ -1,57 +1,58 @@
 <template>
     <InventoryHeader :title="$t('Inventory')">
-        <div class="flex flex-col relative">
+        <div class="inventory-container">
             <InventoryTopBar :craft-filters="getCraftFilters()"
                              @updates-search-value="updateSearchValue"
                              @updates-craft-filters="updateCraftFilters"
                              :crafts="filteredCrafts"/>
             <table>
-                <thead class="sticky z-20 top-0 transition-all duration-300 shadow-sm text-white">
-                <tr class="text-xs">
+                <thead class="inventory-table-head">
+                <tr>
                     <th v-for="(column,index) in columns"
                         :key="column.id"
                         @mouseover="showMenu = column.id"
                         @mouseout="showMenu = null"
                         :class="getColumnCls(index, column) + ' bg-secondary'">
-                        <div class="w-full h-full flex flex-row items-center relative">
-                            <div class="flex flex-row w-full h-full py-2 text-left items-center cursor-pointer">
+                        <div class="inventory-th-container">
+                            <div class="inventory-th-edit-container">
                                 <div
-                                    class="w-[99%] px-2 overflow-hidden overflow-ellipsis whitespace-nowrap"
+                                    class="column-name"
                                     @click="toggleColumnEdit(column)">
                                     {{ column.name }}
                                 </div>
                                 <div
-                                    :class="[column.clicked ? '' : 'hidden', ' bg-secondary flex flex-row items-center px-1 top-[4px] text-white gap-x-2 w-full z-50 absolute']">
+                                    :class="[column.clicked ? '' : '!hidden', ' column-input-container']">
                                     <input
                                         type="text"
                                         :ref="(element) => createDynamicColumnNameInputRef(element, column.id)"
-                                        class="w-full p-1 pl-2 border-0 text-xs text-black"
+                                        class="column-input"
                                         v-model="column.newValue"
                                         @focusout="applyColumnValueChange(column)">
                                 </div>
                             </div>
                             <Menu v-show="showMenu === column.id && !column.showColorMenu"
                                   as="div"
-                                  class="absolute z-50 right-0 cursor-pointer flex flex-row items-center">
+                                  class="inventory-th-menu-container">
                                 <MenuButton as="div">
-                                    <IconDotsVertical class="w-5 h-5 flex-shrink-0 z-50" stroke-width="1.5"
+                                    <IconDotsVertical class="menu-button"
+                                                      stroke-width="1.5"
                                                       aria-hidden="true"/>
                                 </MenuButton>
-                                <div class="w-full h-full relative">
+                                <div class="inventory-th-menu">
                                     <transition enter-active-class="transition ease-out duration-100"
                                                 enter-from-class="transform opacity-0 scale-95"
                                                 enter-to-class="transform opacity-100 scale-100"
                                                 leave-active-class="transition ease-in duration-75"
                                                 leave-from-class="transform opacity-100 scale-100"
                                                 leave-to-class="transform opacity-0 scale-95">
-                                        <MenuItems class="absolute z-50 -translate-x-[110%] top-2 shadow-lg rounded-xl bg-artwork-navigation-background focus:outline-none">
+                                        <MenuItems class="menu-items">
                                             <MenuItem v-slot="{ active }" as="div">
                                                 <a @click="column.showColorMenu = true"
-                                                   :class="[active ? 'rounded-xl bg-artwork-navigation-color/10 text-white' : 'text-secondary', 'cursor-pointer group flex items-center px-4 py-2 subpixel-antialiased']">
+                                                   :class="[active ? 'active' : 'not-active', 'default group']">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                          viewBox="0 0 24 24"
                                                          stroke-width="1.5" stroke="currentColor"
-                                                         class="mr-3 h-5 w-5 text-primaryText group-hover:text-white">
+                                                         class="icon group-hover:text-white">
                                                         <path stroke-linecap="round" stroke-linejoin="round"
                                                               d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"/>
                                                     </svg>
@@ -60,8 +61,8 @@
                                             </MenuItem>
                                             <MenuItem v-slot="{ active }" as="div">
                                                 <a @click="duplicateColumn(column.id)"
-                                                   :class="[active ? 'rounded-xl bg-artwork-navigation-color/10 text-white' : 'text-secondary', 'cursor-pointer group flex items-center px-4 py-2 subpixel-antialiased']">
-                                                    <IconCopy class="mr-3 h-5 w-5 text-primaryText group-hover:text-white"/>
+                                                   :class="[active ? 'active' : 'not-active', 'default group']">
+                                                    <IconCopy class="icon group-hover:text-white"/>
                                                     {{ $t('Duplicate') }}
                                                 </a>
                                             </MenuItem>
@@ -69,8 +70,8 @@
                                                       v-slot="{ active }"
                                                       as="div">
                                                 <a @click="openConfirmDeleteColumnModal(column.id)"
-                                                   :class="[active ? 'rounded-xl bg-artwork-navigation-color/10 text-white' : 'text-secondary', 'cursor-pointer group flex items-center px-4 py-2 subpixel-antialiased']">
-                                                    <IconTrash class="mr-3 h-5 w-5 text-primaryText group-hover:text-white"/>
+                                                   :class="[active ? 'active' : 'not-active', 'default group']">
+                                                    <IconTrash class="icon group-hover:text-white"/>
                                                     {{ $t('Delete') }}
                                                 </a>
                                             </MenuItem>
@@ -78,8 +79,8 @@
                                                       v-slot="{ active }"
                                                       as="div">
                                                 <a @click="openEditColumnSelectOptionsModal(column)"
-                                                   :class="[active ? 'rounded-xl bg-artwork-navigation-color/10 text-white' : 'text-secondary', 'cursor-pointer group flex items-center px-4 py-2 subpixel-antialiased']">
-                                                    <IconEdit class="mr-3 h-5 w-5 text-primaryText group-hover:text-white"/>
+                                                   :class="[active ? 'active' : 'not-active', 'default group']">
+                                                    <IconEdit class="icon group-hover:text-white"/>
                                                     {{ $t('Edit') }}
                                                 </a>
                                             </MenuItem>
@@ -91,24 +92,24 @@
                                      as="div"
                                      v-model="column.background_color">
                                 <ListboxOptions :static="column.showColorMenu"
-                                                class="absolute -translate-x-[107%] translate-y-[8px] z-40 flex flex-col gap-2 p-2  shadow-lg rounded-xl bg-artwork-navigation-background focus:outline-none">
+                                                class="inventory-th-color-menu-container">
                                     <ListboxOption as="template"
                                                    v-for="(color) in columnBgColors"
                                                    :key="color"
                                                    :value="color"
                                                    v-slot="{ active, selected }">
-                                        <li class="cursor-pointer flex flex-row p-2 items-center hover:bg-artwork-navigation-color/10 transition-all duration-300 rounded-md"
+                                        <li class="menu-item"
                                             @click="applyColumnBackgroundColorChange(color, column)">
-                                                <span class="flex rounded-full h-10 w-10 relative" :class="color">
+                                                <span :class="'icon-container ' + color">
                                                      <CheckIcon v-if="selected"
-                                                                :class="'h-5 w-5 flex absolute top-2.5 left-2.5 bg-black bg-opacity-25 p-1 text-white rounded-full'"
+                                                                class="icon"
                                                                 aria-hidden="true"/>
                                                 </span>
                                         </li>
                                     </ListboxOption>
-                                    <li class="cursor-pointer flex flex-row p-2 items-center justify-center hover:bg-artwork-navigation-color/10 transition-all duration-300 rounded-md"
+                                    <li class="menu-item-close"
                                         @click="column.showColorMenu = false;">
-                                        <IconX class="w-4 h-4 text-gray-400"/>
+                                        <IconX class="icon"/>
                                     </li>
                                 </ListboxOptions>
                             </Listbox>
@@ -360,50 +361,4 @@ onUpdated(() => {
     setFilterAndSearchData();
 });
 </script>
-
-<style scoped>
-.whiteColumn {
-    background-color: #FCFCFBFF;
-}
-
-.darkBlueColumn {
-    background-color: #21485C;
-}
-
-.darkGreenColumn {
-    background-color: #4D908E;
-}
-
-.darkLightBlueColumn {
-    background-color: #168FC3;
-}
-
-.lightBlueNew {
-    background-color: #3DC3CB;
-}
-
-.greenColumn {
-    background-color: #2EAA63;
-}
-
-.lightGreenColumn {
-    background-color: #86C554;
-}
-
-.yellowColumn {
-    background-color: #F1B640;
-}
-
-.orangeColumn {
-    background-color: #EB7A3D;
-}
-
-.redColumn {
-    background-color: #DA3F87;
-}
-
-.pinkColumn {
-    background-color: #641A54;
-}
-</style>
 

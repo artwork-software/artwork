16f1bc89d38a8b9559782e082fdd72e5f1065e25
Jonas Glauß <glauss-jonas@web.de>
2024-07-11 17:44:43 +0200

    refs test-pipeline
    - test

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1837,10 +1837,7 @@ class ProjectController extends Controller
         $projectTab->load(['components.component.projectValue' => function ($query) use ($project): void {
             $query->where('project_id', $project->id);
         }, 'components' => function ($query): void {
-            $query->orderBy('order');
-        }, 'sidebarTabs.componentsInSidebar.component.projectValue' => function ($query) use ($project): void {
-            $query->where('project_id', $project->id);
-        }]);
+            $query->orderBy('order');}, 'sidebarTabs.componentsInSidebar.component.projectValue' => function ($query) use ($project): void {$query->where('project_id', $project->id);}]);
 
         $projectTabComponents = $projectTab->components()->with('component')->get()->concat(
             $projectTab->sidebarTabs->flatMap->componentsInSidebar->unique('id')

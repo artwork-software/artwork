577edfdb1a7ee62a7ef8591e42670df7269a18b0
Jonas Glauß <glauss-jonas@web.de>
2024-08-21 10:10:47 +0200

    refs #ARTWORK-150-notifications
    - GlobalNotification tests
    - remove unnecessary UpdateGlobalNotificationImageRequest.php

---
+++ tests/Unit/Artwork/Modules/GlobalNotification/Http/Requests/StoreGlobalNotificationRequestTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit\Artwork\Modules\GlobalNotification\Http\Requests;
+
+use Artwork\Modules\GlobalNotification\Http\Requests\StoreGlobalNotificationRequest;
+use PHPUnit\Framework\TestCase;
+
+class StoreGlobalNotificationRequestTest extends TestCase
+{
+    public function testRules(): void
+    {
+        self::assertSame(
+            [
+                'notificationName' => 'required|string',
+                'notificationDeadlineDate' => 'string|nullable',
+                'notificationDeadlineTime' => 'string|nullable',
+                'notificationDescription' => 'string|nullable',
+            ],
+            (new StoreGlobalNotificationRequest())->rules()
+        );
+    }
+}

8651163b2c637a6ce9860f9e0b6caabf57b7ea8e
Jonas Glauß <glauss-jonas@web.de>
2024-08-30 10:27:13 +0200

    refs #ARTWORK-176-calendar-bulk-edit-changes
    - bulk body changes

--- resources/js/Pages/Projects/Components/BulkComponents/BulkBody.vue
+++ resources/js/Pages/Projects/Components/BulkComponents/BulkBody.vue
@@ -355,15 +355,15 @@ const {hasAdminRole} = usePermission(usePage().props),
         currentSort.value = type;
         if (currentSort.value === 1) {
             events.sort((a, b) => {
-                return a.room.name.localeCompare(b.room.name);
+                return a.room.name.localeCompare(b.room.name) || a.day.localeCompare(b.day) || a.start_time?.localeCompare(b.start_time);
             });
         } else if (currentSort.value === 2) {
             events.sort((a, b) => {
-                return a.type.name.localeCompare(b.type.name);
+                return a.type.name.localeCompare(b.type.name) || a.day.localeCompare(b.day) || a.start_time?.localeCompare(b.start_time);
             });
         } else if (currentSort.value === 3) {
             events.sort((a, b) => {
-                return a.day.localeCompare(b.day);
+                return a.day.localeCompare(b.day) || a.start_time?.localeCompare(b.start_time);
             });
         } else {
             events.sort((a, b) => {

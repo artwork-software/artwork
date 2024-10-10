b8fc1ee835e36cbe34181b0c52016a81bb989730
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2024-10-10 10:07:56 +0200

    add headless ui float

--- resources/js/Components/Menu/BaseMenu.vue
+++ resources/js/Components/Menu/BaseMenu.vue
@@ -1,51 +1,56 @@
 <template>
-    <Menu as="div" class="inline-block" :class="!noRelative ? 'relative' : ''">
-        <div class="flex items-center justify-center w-full font-semibold text-artwork-buttons-context" ref="menuButtonRef">
-            <MenuButton @click="toggleMenu">
-                <IconDotsVertical v-if="!showSortIcon"
-                                  stroke-width="1.5"
-                                  class="flex-shrink-0"
-                                  aria-hidden="true"
-                                  :class="[dotsColor, dotsSize]"
-                />
-                <ToolTipComponent
-                    v-else
-                    direction="bottom"
-                    :tooltip-text="$t('Sorting')"
-                    icon="IconSortDescending"
-                    icon-size="h-8 w-8"
-                    :white-icon="whiteIcon"
-                />
-            </MenuButton>
-        </div>
 
-        <transition enter-active-class="transition ease-out duration-100"
-                    enter-from-class="transform opacity-0 scale-95"
-                    enter-to-class="transform opacity-100 scale-100"
-                    leave-active-class="transition ease-in duration-75"
-                    leave-from-class="transform opacity-100 scale-100"
-                    leave-to-class="transform opacity-0 scale-95">
-            <MenuItems v-if="menuVisible"
-                       class="z-50 rounded-lg bg-artwork-navigation-background shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                       :style="menuStyles" :class="[menuWidth]">
-                <div>
-                    <slot />
-                </div>
-            </MenuItems>
-        </transition>
+    <Menu as="div" class="inline-block" :class="!noRelative ? 'relative' : ''">
+        <Float auto-placement portal  :offset="{ mainAxis: -10, crossAxis: 75}">
+            <div class="font-semibold text-artwork-buttons-context flex items-center justify-center" ref="menuButtonRef">
+                <MenuButton>
+                    <IconDotsVertical
+                        v-if="!showSortIcon"
+                        stroke-width="1.5"
+                        class="flex-shrink-0"
+                        aria-hidden="true"
+                        :class="[dotsColor, dotsSize]"
+                    />
+                    <ToolTipComponent
+                        v-else
+                        direction="bottom"
+                        :tooltip-text="$t('Sorting')"
+                        icon="IconSortDescending"
+                        icon-size="h-8 w-8"
+                        :white-icon="whiteIcon"
+                    />
+                </MenuButton>
+            </div>
+
+            <transition enter-active-class="transition ease-out duration-100"
+                        enter-from-class="transform opacity-0 scale-95"
+                        enter-to-class="transform opacity-100 scale-100"
+                        leave-active-class="transition ease-in duration-75"
+                        leave-from-class="transform opacity-100 scale-100"
+                        leave-to-class="transform opacity-0 scale-95">
+                <MenuItems class="z-50 rounded-lg bg-artwork-navigation-background shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" :class="[menuWidth]">
+                    <div>
+                        <slot />
+                    </div>
+                </MenuItems>
+            </transition>
+        </Float>
     </Menu>
+
 </template>
 
 <script>
-import { defineComponent, ref, onMounted, onBeforeUnmount } from 'vue';
+import { defineComponent } from 'vue';
 import { Menu, MenuButton, MenuItems } from '@headlessui/vue';
 import IconLib from '@/Mixins/IconLib.vue';
 import ToolTipComponent from "@/Components/ToolTips/ToolTipComponent.vue";
+import {Float} from "@headlessui-float/vue";
 
 export default defineComponent({
     name: 'BaseMenu',
     mixins: [IconLib],
     components: {
+        Float,
         ToolTipComponent,
         Menu,
         MenuButton,
@@ -81,76 +86,12 @@ export default defineComponent({
             required: false,
             default: false
         },
+        placement: {
+            type: String,
+            required: false,
+            default: 'top',
+        }
     },
-    setup(props) {
-        const menuButtonRef = ref(null);
-        const menuVisible = ref(false);
-        const menuStyles = ref({});
 
-        const toggleMenu = () => {
-            if (menuVisible.value) {
-                hideMenu();
-            } else {
-                updateMenuPosition();
-            }
-        };
-
-        const updateMenuPosition = () => {
-            const buttonRect = menuButtonRef.value.getBoundingClientRect();
-            const menuWidth = 224;
-            const windowWidth = window.innerWidth;
-
-            let leftPosition;
-
-            if (!props.right) {
-                leftPosition = buttonRect.right - menuWidth;
-                if (leftPosition < 0) {
-                    leftPosition = 0;
-                }
-            } else {
-                leftPosition = buttonRect.left;
-                if (leftPosition + menuWidth > windowWidth) {
-                    leftPosition = windowWidth - menuWidth;
-                }
-            }
-
-            menuStyles.value = {
-                position: 'fixed',
-                top: `${buttonRect.bottom}px`,
-                left: `${leftPosition}px`,
-                zIndex: 50,
-            };
-
-            menuVisible.value = true;
-        };
-
-        const hideMenu = () => {
-            menuVisible.value = false;
-        };
-
-        const handleScrollOrResize = () => {
-            if (menuVisible.value) {
-                updateMenuPosition();
-            }
-        };
-
-        onMounted(() => {
-            window.addEventListener('scroll', handleScrollOrResize);
-            window.addEventListener('resize', handleScrollOrResize);
-        });
-
-        onBeforeUnmount(() => {
-            window.removeEventListener('scroll', handleScrollOrResize);
-            window.removeEventListener('resize', handleScrollOrResize);
-        });
-
-        return {
-            menuButtonRef,
-            menuVisible,
-            menuStyles,
-            toggleMenu,
-            hideMenu,
-        };
-    },
 });
 </script>

5802715d6ac0f48a74714c8fb28042e3d0ac4d7e
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2024-10-17 15:56:45 +0200

    Update 2024_10_17_150257_add_enums_to_shift_plan_user_sort_by.php

--- database/migrations/2024_10_17_150257_add_enums_to_shift_plan_user_sort_by.php
+++ database/migrations/2024_10_17_150257_add_enums_to_shift_plan_user_sort_by.php
@@ -13,6 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('shift_plan_user_sort_by');
             $table->string('shift_plan_user_sort_by')
                 ->after('show_crafts')
                 ->default(null)

c66d61eea0ac1c3777448233b38ef0308c17b7de
Jonas Glauß <glauss-jonas@web.de>
2025-01-23 08:46:50 +0100

    refs #ARTWORK-383-event-property-feature
    - create UpdateUserCalendarFilterEventPropertiesCommand

--- artwork/Modules/UserCalendarFilter/Services/UserCalendarFilterService.php
+++ artwork/Modules/UserCalendarFilter/Services/UserCalendarFilterService.php
@@ -2,9 +2,10 @@
 
 namespace Artwork\Modules\UserCalendarFilter\Services;
 
-use Artwork\Modules\EventProperty\Models\EventProperty;
+use Artwork\Modules\UserCalendarFilter\Models\UserCalendarFilter;
 use Artwork\Modules\UserCalendarFilter\Repositories\UserCalendarFilterRepository;
 use Illuminate\Database\Eloquent\Collection;
+use Throwable;
 
 readonly class UserCalendarFilterService
 {
@@ -17,11 +18,28 @@ readonly class UserCalendarFilterService
         return $this->userCalendarFilterRepository->getAll();
     }
 
-    public function update(): EventProperty
+    /**
+     * @throws Throwable
+     */
+    public function updateEventPropertiesOfAll(array $eventPropertyIds): void
     {
+        foreach ($this->getAll() as $userCalendarFilter) {
+            $this->updateOrFail(
+                $userCalendarFilter,
+                ['event_properties' => $eventPropertyIds]
+            );
+        }
     }
 
-    public function updateEventProperties(array $eventPropertyIds): void
-    {
+    /**
+     * @throws Throwable
+     */
+    public function updateOrFail(
+        UserCalendarFilter $userCalendarFilter,
+        array $attributes
+    ): UserCalendarFilter {
+        $this->userCalendarFilterRepository->updateOrFail($userCalendarFilter, $attributes);
+
+        return $userCalendarFilter;
     }
 }

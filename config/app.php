e19f7e293e2f402f9bee551789b42e85ca6661f7
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2025-02-12 15:30:44 +0100

    disable update page if has no notion api token

--- config/app.php
+++ config/app.php
@@ -17,6 +17,8 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+
+    'notion_api_token' => env('NOTION_API_TOKEN', ''),
     /*
     |--------------------------------------------------------------------------
     | Application Environment

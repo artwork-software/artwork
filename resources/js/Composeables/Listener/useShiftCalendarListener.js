072573610c8ab20605964634b677503a7fed1feb
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2025-02-24 14:12:05 +0100

    fix broadcast for multi delete uni shifts

--- resources/js/Composeables/Listener/useShiftCalendarListener.js
+++ resources/js/Composeables/Listener/useShiftCalendarListener.js
@@ -117,8 +117,8 @@ export function useShiftCalendarListener(newShiftPlanData) {
     }
 
     function removeShiftFromRoomAndEvents(data) {
-        const { shift, room_id } = data;
-        const room = findRoomById(room_id);
+        const { shift, roomId } = data;
+        const room = findRoomById(roomId);
         if (!room) return;
 
         shift.daysOfShift.forEach((day) => {
@@ -170,6 +170,7 @@ export function useShiftCalendarListener(newShiftPlanData) {
 
             Echo.private('destroy.events.room.' + room.roomId)
                 .listen('.shift-destroyed.in.event', (data) => {
+                    console.log('hier angekommen', data);
                     removeShiftFromRoomAndEvents(data);
                 });
 

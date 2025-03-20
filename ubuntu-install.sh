159dfc0fc04a716b036c3248f69fda99346d54b0
updatedData <jason@updated-data.me>
2025-03-20 16:53:49 +0100

    #1063-installer fixes

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -96,7 +96,7 @@ sudo apt-get install -y php8.3-cli php8.3-dev php8.3-fpm \
        php8.3-imap php8.3-mysql php8.3-mbstring \
        php8.3-xml php8.3-zip php8.3-bcmath php8.3-soap \
        php8.3-intl php8.3-readline \
-       php8.3-ldap \ apparmor-utils \
+       php8.3-ldap \
        php8.3-msgpack php8.3-igbinary php8.3-redis php8.3-swoole \
        php8.3-memcached php8.3-pcov \
        meilisearch=1.9.* \

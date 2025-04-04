aa77beaac961d33bf2516ef3308b624c9741352d
Jason Roeser <jason@updated-data.me>
2025-04-04 11:26:59 +0200

    ARTWORK-111 - removed prunable

--- artwork/Modules/Availability/Models/AvailabilitySeries.php
+++ artwork/Modules/Availability/Models/AvailabilitySeries.php
@@ -4,9 +4,7 @@ namespace Artwork\Modules\Availability\Models;
 
 use Artwork\Core\Database\Models\Model;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
@@ -17,7 +15,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class AvailabilitySeries extends Model
 {
     use HasFactory;
-    use Prunable;
 
     protected $fillable = [
         'frequency',
@@ -27,10 +24,5 @@ class AvailabilitySeries extends Model
     public function availabilities(): HasMany
     {
         return $this->hasMany(Availability::class, 'series_id', 'id')->without('series');
-    }
-
-    public function prunable(): Builder
-    {
-        return static::whereDoesntHave('availabilities');
     }
 }

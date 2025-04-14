954474cbd1a9110e0770569a69edc334e004fee7
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2025-04-14 14:34:33 +0200

    Redesign base, fix filters

--- resources/js/Pages/Inventory/LayoutComponents/InventoryFilterComponent.vue
+++ resources/js/Pages/Inventory/LayoutComponents/InventoryFilterComponent.vue
@@ -13,39 +13,55 @@
                     <div>
                         <label class="font-lexend text-xs mb-1">{{ filterProperty.name }}</label>
                     </div>
-                    <div class="flex items-center border border-gray-200 rounded-lg focus-within:ring-2 focus-within:ring-blue-500">
+                    <div class="flex items-center border border-gray-200 rounded-lg focus-within:ring-2 focus-within:ring-blue-500" v-if="filterProperty.type !== 'selection' && filterProperty.type !== 'checkbox'">
                         <select v-model="filterProperty.operator" v-if="getAllowedFilters(filterProperty.type).length > 0" class="text-gray-700 min-w-28 text-sm px-2 py-2 border-none rounded-l-lg focus:outline-none focus:ring-0">
                             <option v-for="filter in getAllowedFilters(filterProperty.type)" :key="filter.type" :value="filter.type">
                                 {{ filter.name }}
                             </option>
                         </select>
                         <input
-                            v-if="filterProperty.type !== 'selection'"
                             v-model="filterProperty.value"
                             :type="filterProperty.type"
-                            class="w-full px-3 py-2 xsDark placeholder:xsLight h-10 rounded-lg border-none focus:outline-none focus:ring-0 checked:text-green-500"
+                            class="w-full px-3 py-2 xsDark placeholder:xsLight shadow-sm h-10 rounded-lg border-none focus:outline-none focus:ring-0 checked:text-green-500"
                             :placeholder="filterProperty.name"
                         />
-                        <div v-else class="w-full h-full">
-                            <select id="location" name="location" v-model="filterProperty.value" class="block w-full h-10 rounded-md bg-white border-none text-xs text-gray-900 outline-0 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-0 ring-0 focus:ring-0">
-                                <option v-for="value in filterProperty.select_values" :value="value" :key="value">{{ value }}</option>
-                            </select>
+
+                    </div>
+                    <div v-if="filterProperty.type === 'selection'" class="w-full">
+                        <select id="location" name="location" @change="submitFilter" v-model="filterProperty.value" class="block shadow-sm w-full h-10 rounded-lg border border-gray-200 bg-white text-xs text-gray-900 outline-0 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-0 ring-0 focus:ring-0">
+                            <option v-for="value in filterProperty.select_values" :value="value" :key="value">{{ value }}</option>
+                        </select>
+                    </div>
+                    <div v-if="filterProperty.type === 'checkbox'" class="w-full h-full mt-1.5">
+                        <div class="flex gap-3">
+                            <div class="flex h-6 shrink-0 items-center">
+                                <div class="group grid size-4 grid-cols-1">
+                                    <input v-model="filterProperty.value" @change="submitFilter" id="comments" aria-describedby="comments-description" name="comments" type="checkbox" checked="" class="input-checklist" />
+                                </div>
+                            </div>
+                            <div class="text-sm/6">
+                                <label for="comments" class="font-medium text-gray-900">{{ filterProperty.name }}</label>
+                            </div>
                         </div>
                     </div>
                 </div>
 
             </div>
-            <div class="mt-3 flex justify-between">
+            <div class="mt-3 flex items-center gap-x-5">
                 <SmallFormButton type="button" @click="submitFilter">
                     {{ $t('Apply Filter') }}
                 </SmallFormButton>
+
+                <SmallFormButton v-if="checkIfAnyFilterIsSet" type="button" @click="resetFilter">
+                    {{ $t('Reset') }}
+                </SmallFormButton>
             </div>
         </div>
-        <div class="my-3">
+        <div class="my-3 flex flex-wrap gap-2" v-if="newFilterObject.length > 0" >
             <!-- filter Tags -->
-            <div v-if="newFilterObject.length > 0" v-for="(filter, index) in newFilterObject" :key="index" class="flex flex-wrap gap-2">
+            <div v-for="(filter, index) in newFilterObject" :key="index" >
                 <div v-if="filter.value" class="flex items-center bg-blue-50 rounded-full px-3 py-1 text-sm font-medium text-blue-700 border border-blue-100">
-                    <span>{{ filter.name }}: {{ filter.value }}</span>
+                    <span>{{ filter.name }}{{ isBool(filter.value) ? '' : ': ' + filter.value }}</span>
                     <button type="button" @click="removeFilter(filter)" class="ml-2 text-blue-500 hover:text-blue-700">
                         <component is="IconX" class="size-4" />
                     </button>
@@ -57,10 +73,11 @@
 
 <script setup>
 
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import SmallFormButton from "@/Components/Buttons/SmallFormButton.vue";
 import TinyPageHeadline from "@/Components/Headlines/TinyPageHeadline.vue";
 import {router, usePage} from "@inertiajs/vue3";
+import {isBool} from "@aesoper/normal-utils";
 
 const props = defineProps({
     filterableProperties: {
@@ -123,7 +140,7 @@ onMounted(() => {
 
 const submitFilter = () => {
     const cleanFilters = newFilterObject.value
-        .filter(f => f.value !== '' && f.value !== null)
+        .filter(f => f.value !== '' && f.value !== null && f.value !== false)
         .map(f => ({
             property_id: f.id,
             operator: f.operator,
@@ -137,6 +154,18 @@ const submitFilter = () => {
     });
 }
 
+const checkIfAnyFilterIsSet = computed(() => {
+    return newFilterObject.value.some(filter => {
+        if (filter.type === 'checkbox') {
+            return filter.value;
+        } else if (filter.type === 'selection') {
+            return filter.value !== '';
+        } else {
+            return filter.value !== '' && filter.value !== null && filter.value !== false;
+        }
+    });
+})
+
 const getAllowedFilters = (type) => {
     if (!type) return [];
     return filterTypes.filter(f => f.allowedTypes.includes(type));
@@ -147,6 +176,18 @@ const removeFilter = (filter) => {
     submitFilter()
 }
 
+const resetFilter = () => {
+    newFilterObject.value.forEach(filter => {
+        filter.value = '';
+        filter.operator = 'like';
+    });
+
+    router.reload({
+        data: {
+            filters: JSON.stringify([]),
+        },
+    });
+}
 </script>
 
 <style scoped>

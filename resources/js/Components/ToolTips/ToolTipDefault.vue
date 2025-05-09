a9577642acda59b4ae284442924f20423a87f253
Muelle96 <janni.mueller@yahoo.de>
2025-05-09 12:57:19 +0200

    MainNav-menu now is smaller on smaller screens improvement of Project-Schedule UI fixed wrong Tooltip-Directions deleted "relevant for project period" from Project-View in Calendar because of too much confusion. Now it shows the period for all events of the project

--- resources/js/Components/ToolTips/ToolTipDefault.vue
+++ resources/js/Components/ToolTips/ToolTipDefault.vue
@@ -24,6 +24,10 @@ export default {
             type: Boolean,
             default: false
         },
+        bottom: {
+            type: Boolean,
+            default: false
+        },
         showBackgroundIcon: {
             type: Boolean,
             default: false
@@ -53,7 +57,7 @@ export default {
 </script>
 
 <template>
-    <div class="relative flex items-center z-10">
+    <div class="relative flex items-center z-50">
         <!-- Button oder Icon, das den Tooltip triggert -->
         <button @mouseover="show = true" @mouseleave="show = false" class="focus:outline-none" :class="classes">
             <!-- Ihr SVG-Icon -->
@@ -64,14 +68,18 @@ export default {
             <IconClock24 stroke-width="1.5" class="h-6 w-6" aria-hidden="true" v-if="show24HIcon" :class="iconClasses" />
         </button>
         <!-- Tooltip-Text, der beim Hover erscheint -->
-        <div v-if="show && top" class="absolute z-10 -top-3 text-center w-64 p-2 text-sm leading-tight text-white bg-black rounded-md shadow-lg transform -translate-x-1/2 -translate-y-full left-1/2">
+        <div v-if="show && top" class="absolute z-50 -top-3 text-center w-64 p-2 text-sm leading-tight text-white bg-black rounded-md shadow-lg transform -translate-x-1/2 -translate-y-full left-1/2">
             {{ tooltipText }}
             <!-- Tooltip Pfeil unten -->
             <div class="absolute bg-black h-3 w-3 transform rotate-45 left-1/2 -translate-x-1/2 -bottom-1.5"></div>
         </div>
-        <div v-if="show && left" class="absolute z-10 w-64 p-2 text-sm leading-tight text-white bg-black rounded-md shadow-lg transform -translate-y-1/2 right-full mr-3 top-1/2">
+        <div v-if="show && left" class="absolute z-50 w-64 p-2 text-sm leading-tight text-white bg-black rounded-md shadow-lg transform -translate-y-1/2 right-full mr-3 top-1/2">
             {{ tooltipText }}
             <div class="absolute bg-black h-3 w-3 transform rotate-45 left-full -translate-x-1/2 top-1/2 -mt-1.5"></div>
+        </div>
+        <div v-if="show && bottom" class="absolute z-50 -bottom-3 w-64  text-center p-2 text-sm leading-tight text-white bg-black rounded-md shadow-lg transform -translate-x-1/2 translate-y-full left-1/2">
+            {{ tooltipText }}
+            <div class="absolute bg-black h-3 w-3 transform rounded-sm rotate-45 left-1/2 -translate-x-1/2 -top-1.5"></div>
         </div>
     </div>
 </template>

f42eb4dd8c68923d6ed576a79e28d2b4cd6b80ce
Jason Roeser <jason@updated-data.me>
2025-06-28 20:45:53 +0200

    ARTWORK-174 - fix already present settings

--- database/settings/2025_06_10_000000_initialize_missing_general_settings.php
+++ database/settings/2025_06_10_000000_initialize_missing_general_settings.php
@@ -1,5 +1,6 @@
 <?php
 
+use Spatie\LaravelSettings\Exceptions\SettingAlreadyExists;
 use Spatie\LaravelSettings\Migrations\SettingsMigration;
 
 return new class extends SettingsMigration
@@ -7,22 +8,39 @@ return new class extends SettingsMigration
     public function up(): void
     {
         // Add missing email settings
-        $this->migrator->add('general.invitation_email', '');
-        $this->migrator->add('general.business_email', '');
+        $this->addIfNotExists('general.invitation_email', '');
+        $this->addIfNotExists('general.business_email', '');
 
         // Add budget account management setting
-        $this->migrator->add('general.budget_account_management_global', false);
+        $this->addIfNotExists('general.budget_account_management_global', false);
 
         // Add file mime types settings
-        $this->migrator->add('general.allowed_project_file_mimetypes', ['*']);
-        $this->migrator->add('general.allowed_room_file_mimetypes', ['*']);
-        $this->migrator->add('general.allowed_branding_file_mimetypes', ['*']);
-        $this->migrator->add('general.allowed_contract_file_mimetypes', ['*']);
+        $this->addIfNotExists('general.allowed_project_file_mimetypes', ['*']);
+        $this->addIfNotExists('general.allowed_room_file_mimetypes', ['*']);
+        $this->addIfNotExists('general.allowed_branding_file_mimetypes', ['*']);
+        $this->addIfNotExists('general.allowed_contract_file_mimetypes', ['*']);
 
         // Add file size settings (in MB)
-        $this->migrator->add('general.allowed_project_file_size', 150);
-        $this->migrator->add('general.allowed_room_file_size', 150);
-        $this->migrator->add('general.allowed_branding_file_size', 150);
-        $this->migrator->add('general.allowed_contract_file_size', 150);
+        $this->addIfNotExists('general.allowed_project_file_size', 150);
+        $this->addIfNotExists('general.allowed_room_file_size', 150);
+        $this->addIfNotExists('general.allowed_branding_file_size', 150);
+        $this->addIfNotExists('general.allowed_contract_file_size', 150);
+    }
+
+    /**
+     * Add a setting only if it doesn't already exist
+     *
+     * @param string $property
+     * @param mixed $value
+     * @param bool $encrypted
+     * @return void
+     */
+    private function addIfNotExists(string $property, $value = null, bool $encrypted = false): void
+    {
+        try {
+            $this->migrator->add($property, $value, $encrypted);
+        } catch (SettingAlreadyExists $e) {
+            // Setting already exists, do nothing
+        }
     }
 };

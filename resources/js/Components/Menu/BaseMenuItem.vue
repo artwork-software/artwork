a06fbdb2515150524648bcbcc95fc92595eeb2a0
Timo Deeken <timodeeken@web.de>
2025-07-23 09:48:07 +0200

    first part fixes

--- resources/js/Components/Menu/BaseMenuItem.vue
+++ resources/js/Components/Menu/BaseMenuItem.vue
@@ -1,7 +1,7 @@
 <template>
     <MenuItem v-slot="{ active }">
         <Link :href="link" v-if="asLink"
-           :class="[whiteMenuBackground ? 'hover:text-artwork-buttons-create text-gray-500' : 'hhover:text-white text-white', 'group flex items-center px-4 py-2 text-sm subpixel-antialiased cursor-pointer rounded-3xl']">
+           :class="[whiteMenuBackground ? 'hover:text-artwork-buttons-create text-gray-500' : 'hover:text-white text-white', 'group flex items-center px-4 py-2 text-sm subpixel-antialiased cursor-pointer rounded-lg hover:bg-gray-100']">
             <component :is="icon" class="mr-3 h-5 w-5 text-primaryText" :class="whiteMenuBackground ? 'group-hover:text-artwork-buttons-create text-gray-500' : ' group-hover:text-artwork-buttons-hover text-gray-400'" aria-hidden="true"/>
             {{ withoutTranslation ? title : $t(title) }}
         </Link>

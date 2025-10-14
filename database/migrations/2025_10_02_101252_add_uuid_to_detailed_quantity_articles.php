fe1c6a33f0935f0c7d07a00a170fb16119746bbf
Jason Roeser <jason@updated-data.me>
2025-10-14 22:20:27 +0200

    hotfix for breaking migration

--- database/migrations/2025_10_02_101252_add_uuid_to_detailed_quantity_articles.php
+++ database/migrations/2025_10_02_101252_add_uuid_to_detailed_quantity_articles.php
@@ -2,18 +2,26 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('inventory_detailed_quantity_articles', function (Blueprint $table) {
-            $table->uuid('type_number')->after('id')->unique();
-        });
+        if (!Schema::hasColumn('inventory_detailed_quantity_articles', 'type_number')) {
+            Schema::table('inventory_detailed_quantity_articles', function (Blueprint $table) {
+                $table->uuid('type_number')->nullable()->after('id');
+            });
+
+            DB::statement('UPDATE inventory_detailed_quantity_articles SET type_number = UUID() WHERE type_number IS NULL OR type_number = ""');
+
+            Schema::table('inventory_detailed_quantity_articles', function (Blueprint $table) {
+                $table->unique('type_number', 'inventory_detailed_quantity_articles_type_number_unique');
+            });
+        }
     }
 
     /**
